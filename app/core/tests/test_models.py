3a26a8f026ebb06cbd636fead29642d6b2ee897c
nnamso <macbook@MacBooks-MacBook-Pro.local>
2020-04-11 15:40:46 +0100

    Added user and super user

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_succesful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@nedemenang.com'
+        password = 'Password1'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalize(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@NEDEMENANG.COM'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='randomstring'
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                email=None,
+                password='randomstring'
+            )
+
+    def test_create_new_superuser(self):
+        """Test creating a new super user"""
+        user = get_user_model().objects.create_superuser(
+            'test@nedemenang@gmail.com',
+            'randompassword'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+
+
